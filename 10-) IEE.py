7c60c4ebd21048617271a85517dcda819401b10d
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2022-09-24 23:11:02 +0300

    s
    s

---
+++ 10-) IEE.py
@@ -0,0 +1,74 @@
+#******************************** SAYISAL VERİ TİPLERİ VE SAYISAL OPERATÖRLER ***************************************
+# 1-) SAYISAL VERİ TİPLERİ
+
+# - Integer (int)
+
+# - Float 
+
+# 2-) SAYISAL OPERATÖRLER
+
+# - Toplama (+)
+
+# - Çıkarma (-)
+
+# - Çarpma  (*)
+
+# - Bölme   (/)
+
+# - Mod Alma (%)
+
+# - Üs Alma (**)
+
+# 3-) SAYISAL VERİ TİPLERİ ARASI İŞLEMLER
+
+
+
+
+
+#****************************************** DEĞİŞKENLER *************************************************************
+# 1-) Değişken Nedir?
+
+# 2-) Değişken tanımlama Kuralları Nelerdir? 
+
+# - Sayı ile Başlayamaz
+
+# - Boşluk İçeremez
+
+# - Büyük Küçük Harf Farklılığı Vardır
+
+# - Yan Yana Değer Ataması Yapılabilir
+
+# - Türkçe Karakter İçermese Güzel Olur
+
+# 3-) Değişkenlerle İlgili Uygulamalar
+
+
+
+
+
+
+#****************************************** STRİNGLER *************************************************************
+# 1-) String Veri Tipi Nedir
+
+# 2-) String İşlemleri Nelerdir (İndeks Kavramı)
+
+# 3-) Stringle İlgili Fonksiyonlar
+
+#  - Len (Uzunluk Fonksiyonu)
+
+#  - Upper ve Lower Fonksiyonları 
+
+#  - Find Fonksiyonu
+
+#  - Startswith ve Endswith Fonksiyonları
+
+#  - Split Fonksiyonu (Ayrıma)
+
+#  - Replace Fonksiyonu (Cümle Değiştirme)
+#****************************************** LİSTELER *************************************************************
+#****************************************** OPERATÖRLER *************************************************************
+#****************************************** KOŞULLU İFADELER *************************************************************
+#****************************************** DÖNGÜLER *************************************************************
+#****************************************** FONKSİYONLAR *************************************************************
+#****************************************** NESNE TABANLI PROGRAMLAMA *************************************************************
+#****************************************** MODÜLLER *************************************************************
